097be68d765de2dfec79680dc6b5e56dd9696c96
Muzikanto <schiriy_maxim@icloud.com>
2023-01-01 19:50:45 +0300

    fixes

--- main/src/pages/_app.tsx
+++ main/src/pages/_app.tsx
@@ -2,9 +2,11 @@ import type { AppProps } from 'next/app'
 import React from "react";
 import {DefaultSeo} from "next-seo";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps & {Component: {getLayout?: React.FC}}) {
+  const getLayout = Component.getLayout || ((page) => page)
+
   return <>
     <DefaultSeo/>
-    <Component {...pageProps} />
+    {getLayout(<Component {...pageProps} />)}
     </>
 }
---
+++ main/src/utils/next.ts
@@ -0,0 +1,3 @@
+import {NextPage} from "next";
+
+export type AppPage = NextPage & {getLayout?: (children: JSX.Element) => JSX.Element};
